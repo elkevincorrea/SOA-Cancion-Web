e96604dee27e8afc25b728d21aaeddfc48ddbe62
Hector <hejotru@outlook.com>
2017-10-28 17:15:52 -0500

    feat Vista Agregar Cancion

--- cancionduff/views.py
+++ cancionduff/views.py
@@ -1,6 +1,10 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='templates/index.jinja2')
 def my_view(request):
     return {'project': 'cancionduff'}
+
+@view_config(route_name='cancion-nueva', renderer='templates/agregar.jinja2')
+def cancion_nueva(request):
+    return { }
