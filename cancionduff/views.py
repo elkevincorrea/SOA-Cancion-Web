576cc296f70908dd4e09861d2724164f2255d7e9
Hector <hejotru@outlook.com>
2017-10-28 21:40:05 -0500

    feat 3 Views (Actualizar,Consultar,Eliminar)

--- cancionduff/views.py
+++ cancionduff/views.py
@@ -8,3 +8,15 @@ def my_view(request):
 @view_config(route_name='cancion-nueva', renderer='templates/agregar.jinja2')
 def cancion_nueva(request):
     return { }
+
+@view_config(route_name='cancion-eliminar', renderer='templates/eliminar.jinja2')
+def cancion_eliminar(request):
+    return { }
+
+@view_config(route_name='cancion-actualizar', renderer='templates/actualizar.jinja2')
+def cancion_actualizar(request):
+    return { }
+
+@view_config(route_name='cancion-consultar', renderer='templates/consultar.jinja2')
+def cancion_consultar(request):
+    return { }
