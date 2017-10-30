21695a640e7b1950f58f490b28c2a421ae04c458
Kevin Joan Correa Del Castillo <kevinjoan16@gmail.com>
2017-10-29 22:49:40 -0500

    feat Agregar y listar conectados al servicio Web

--- cancionduff/views.py
+++ cancionduff/views.py
@@ -1,5 +1,9 @@
 from pyramid.view import view_config
+from zeep import Client
+from datetime import datetime
 
+cliente = Client('http://localhost:7101/AplicacionServidorCancion-ProyectoServidorCancion-context-root/CancionSWPort?WSDL');
+factory = cliente.type_factory('ns0')
 
 @view_config(route_name='home', renderer='templates/index.jinja2')
 def my_view(request):
@@ -7,7 +11,19 @@ def my_view(request):
 
 @view_config(route_name='cancion-nueva', renderer='templates/agregar.jinja2')
 def cancion_nueva(request):
-    return { }
+    if request.POST:
+        id = request.POST['id']
+        titulo = request.POST['titulo']
+        nombre_artista = request.POST['nombreArtista']
+        genero = request.POST['genero']
+        fecha_lanzamiento = request.POST['fecha']
+        fecha_lanzamiento = datetime.strptime(fecha_lanzamiento, '%Y-%m-%d')
+        cancion = factory.cancion(id = id, titulo = titulo, nombreArtista = nombre_artista, genero = genero, fechaLanzamiento = fecha_lanzamiento)
+        try:
+            cliente.service.adicionarCancion(cancion)
+        except Exception as error:
+            return {'error': error}
+    return {}
 
 @view_config(route_name='cancion-eliminar', renderer='templates/eliminar.jinja2')
 def cancion_eliminar(request):
@@ -23,7 +39,12 @@ def cancion_consultar(request):
 
 @view_config(route_name='cancion-listar', renderer='templates/listar.jinja2')
 def cancion_listar(request):
-    return { }
+    try:
+        canciones = cliente.service.listarCanciones()
+    except Exception as error:
+        return {'canciones': []}
+    else:
+        return { 'canciones': canciones}
 
 @view_config(route_name='cancion-principal', renderer='templates/index.jinja2')
 def cancion_principal(request):
